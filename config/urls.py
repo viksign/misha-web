86db9bd2089155abf3d541cd13a28fd75d3e72f3
Vikash Seesaha <vikash@seekoo.co.uk>
2026-09-21 01:35:32 +0100

    Allowa all host

--- config/urls.py
+++ config/urls.py
@@ -3,8 +3,10 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import include, path
+from django.views.static import serve
 
 urlpatterns = [
+    path('.well-known/acme-challenge/<path:path>', serve, {'document_root': '/var/www/certbot'}),
     path('admin/', admin.site.urls),
     path('', include('shop.urls')),
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
