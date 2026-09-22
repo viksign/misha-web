49bd8e54982338c446a72014cc349c9ac36af7fc
Vikash Seesaha <vikash@seekoo.co.uk>
2026-09-22 18:00:22 +0100

    new whitenoise middlewareg

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "whitenoise.middleware.WhiteNoiseMiddleware",ROOT_URLCONF = 'config.urls'
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+]
+ROOT_URLCONF = 'config.urls'
 TEMPLATES = [{
     'BACKEND': 'django.template.backends.django.DjangoTemplates',
     'DIRS': [BASE_DIR / 'templates'],
