174ea986b6d4bb86e06f4ef240a608e11fd474d5
Vikash Seesaha <vikash@seekoo.co.uk>
2026-09-22 17:53:39 +0100

    new certbot config

--- config/settings.py
+++ config/settings.py
@@ -26,8 +26,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
-ROOT_URLCONF = 'config.urls'
+    "whitenoise.middleware.WhiteNoiseMiddleware",ROOT_URLCONF = 'config.urls'
 TEMPLATES = [{
     'BACKEND': 'django.template.backends.django.DjangoTemplates',
     'DIRS': [BASE_DIR / 'templates'],
